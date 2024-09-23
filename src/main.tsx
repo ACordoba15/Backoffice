6ddc2a0538370b94fd63227f64630d81d22693cf
andrescr <jcordoba@teledolar.com>
2024-09-23 10:02:22 -0600

    Se agregan las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import { Navbar } from './components/Navbar/Navbar'
 import { Footer } from './components/Footer/Footer'
 import { Table } from './components/Table/Table'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+
+import './index.css'
+import { Record } from './pages/Record'
+import { User } from './pages/User'
+
+// Rutas
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>,
+        errorElement: <div>404 Not Found</div>
+    },
+    {
+        path: '/Records',
+        element: <Record/>,
+        errorElement: <div>404 Not Found</div>
+    },
+    {
+        path: '/Users',
+        element: <User/>,
+        errorElement: <div>404 Not Found</div>
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <div className='main-container'>
-      <Navbar/>
-      <Table/>
-      <Footer/>
-    </div>
-  </StrictMode>,
+    <StrictMode>
+        <div className='main-container'>
+            <Navbar />
+            <RouterProvider router={router} />
+            <Footer />
+        </div>
+    </StrictMode>,
 )
---
+++ src/pages/Record.tsx
@@ -0,0 +1,7 @@
+import { Table } from "../components/Table/Table"
+
+export const Record = () => {
+    return (
+        <div><Table /></div>
+    )
+}
