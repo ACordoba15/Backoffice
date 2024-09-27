a5565f3a1c0a37eede0ae4f3c1f4cd1ab454664e
andrescr <jcordoba@teledolar.com>
2024-09-27 00:03:52 -0600

    Se agrega el diseño a la vista home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,40 @@
-export const Home = () => {
+import { useEffect, useState } from "react";
+import { Card } from "../components/Cards/Cards";
+import axios from "axios";
+import { Record } from "../types/Record";
+
+export function Home() {
+    const base: string = "http://localhost:8000/api";
+    const [totalUsers, setTotalUsers] = useState<number>(0);
+    const [totalRecords, settotalRecords] = useState<number>(0);
+
+    useEffect(() => {
+        axios.get(`${base}/user/all`)
+            .then(response => {
+                setTotalUsers(response.data.length)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        
+        axios.get(`${base}/record/all`)
+            .then(response => {
+                settotalRecords(response.data.length)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
     return (
-        <div className="content">Bienvenido</div>
+        <div className="relative overflow-x-auto overflow-y-auto rounded-lg m-4 content">
+            <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
+            <h1 className="text-2xl font-bold mb-4 text-orange-500">Dashboard</h1>
+            <div>
+                <Card title="Total de Usuarios" value={totalUsers} path="/Users"/>
+                <Card title="Total de Registros" value={totalRecords} path="/Records"/>
+            </div>
+            </div>
+        </div>
     )
 }
---
+++ src/types/Card.ts
@@ -0,0 +1,7 @@
+import { Record } from "./Record";
+
+export type CardType = {
+    title: string;
+    value: number;
+    path: string;
+}
